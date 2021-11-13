1f4bc4e3726571e52c2fabc85b6ab4356f7daec2
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2021-11-13 22:42:15 +0200

    создает галерею, добавляет модалку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// Change code below this line
+const imageGallery = document.querySelector('.gallery');
+const imageList = createGalleryItems(galleryItems);
 console.log(galleryItems);
+
+
+function createGalleryItems(images) {
+    return images.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+        </a>
+        </div>`;
+    }).join('');
+    
+    }
+
+
+imageGallery.insertAdjacentHTML('afterbegin', imageList);
+
+imageGallery.addEventListener('click', getOriginalImage);
+
+function getOriginalImage(event) {
+    event.preventDefault();
+
+    if (!event.target.className === 'gallery-item') {
+        return;
+    }
+    else {
+        const instance = basicLightbox.create(`
+                <img src="assets/images/image.png" width="800" height="600">
+            `)
+        instance.show(() => );
+ 
+    }
+}
